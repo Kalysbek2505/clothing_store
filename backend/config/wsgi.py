83f1b581124fd43310f486eb48db214015ef5e71
xyzwerttt <zarypbekov.kalys@gmail.com>
2025-08-08 12:15:21 +0600

    .

--- backend/config/wsgi.py
+++ backend/config/wsgi.py
@@ -8,9 +8,22 @@ https://docs.djangoproject.com/en/5.1/howto/deployment/wsgi/
 """
 
 import os
-
 from django.core.wsgi import get_wsgi_application
+from django.contrib.auth import get_user_model
+from django.db import OperationalError
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "config.settings")
+# Автосоздание суперюзера
+User = get_user_model()
+username = os.getenv('ADMIN_USER')
+email    = os.getenv('ADMIN_EMAIL')
+password = os.getenv('ADMIN_PASSWORD')
+
+if username and email and password:
+    try:
+        if not User.objects.filter(username=username).exists():
+            User.objects.create_superuser(username, email, password)
+    except OperationalError:
+        pass
 
 application = get_wsgi_application()
+
