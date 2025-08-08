e0267920d05ed8e8d42cc35f296a7ce01659f8b8
xyzwerttt <zarypbekov.kalys@gmail.com>
2025-08-08 12:45:11 +0600

    .

--- backend/backend/apps.py
+++ backend/backend/apps.py
@@ -6,15 +6,20 @@ class BackendConfig(AppConfig):
     name = 'backend'
 
     def ready(self):
-        from django.conf import settings
         from django.contrib.auth import get_user_model
+        from django.conf import settings
 
         def create_admin(sender, **kwargs):
+            # только в проде, DEBUG=False
+            if settings.DEBUG:
+                return
+
             User = get_user_model()
-            if settings.DEBUG is False:
-                env = os.environ
-                u, e, p = env.get('ADMIN_USER'), env.get('ADMIN_EMAIL'), env.get('ADMIN_PASSWORD')
-                if u and p and not User.objects.filter(username=u).exists():
-                    User.objects.create_superuser(u, e or '', p)
+            u = os.getenv('ADMIN_USER')
+            e = os.getenv('ADMIN_EMAIL')
+            p = os.getenv('ADMIN_PASSWORD')
+
+            if u and p and not User.objects.filter(username=u).exists():
+                User.objects.create_superuser(u, e or '', p)
 
         post_migrate.connect(create_admin, sender=self)
